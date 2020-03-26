a437a87ed69dfb6b1ad0a178a8c440b71a56523d
clair5610 <58032072+clair5610@users.noreply.github.com>
2020-03-26 20:06:27 +0800

    程式學習

---
+++ HC_FirstGame_202003/Assets/Scripts/LearnMemberStatic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class LearnMemberStatic : MonoBehaviour
+{
+    // 定義屬性
+    // 語法 : 類型 名稱 指定(=) 值;
+    float a = 0.5f;               // float 浮點數(小數點，數字後須加f)
+    int b = 100;                  // int   整數
+    bool c = true;                // bool  布林植(是否) 是
+    bool d = false;               // 否
+    string e = "我是字串";        // string 字串
+
+    // 修飾詞 類型 名稱 指定 值;
+    // 公開 public 允許所有文件()存取 : 顯示在屬性面板(Unity右邊板塊)
+    public int score = 10;
+    // 私人 private 僅限此類別(腳本)存取 : 隱藏在屬性面板
+    private int speed = 99;
+
+
+    private void Start()
+    {
+        // 使用靜態成員
+        // 靜態屬性
+        // 取得 Get : 類別.靜態屬性
+        print(Random.value);   // 隨機出0.0~1.0之間的數值，常用於攻擊傷害暴擊之類
+        // 設定 Set : 類別.靜態屬性 = 值
+        // 不能設定 Unity api (官網) 中描述後邊帶有 Read Only 的屬性
+        Time.timeScale = 0.5f; // 時間速度變為0.5，也就是變慢為一半
+
+        // 使用靜態方法
+        print(Random.Range(0.1f,0.99f));  // 第一個多載 (可用於傷害(暴擊) or 掉寶機率等)
+        print(Random.Range(100,500));     // 第二個多載
+    }
+}
